e23e35fafa50f511ec9d2088a7c18c35e930b6ec
Adam Sànchez Portero <adam2003sanchez@gmail.com>
2022-10-15 20:01:32 +0200

    pantalla de login acabada

--- app/src/main/java/com/example/zenhabit/MainActivity.kt
+++ app/src/main/java/com/example/zenhabit/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.zenhabit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //Hide action Bar
+
+
         setContentView(R.layout.activity_main)
+
+
     }
 }
