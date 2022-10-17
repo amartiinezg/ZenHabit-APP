d8fa0b0aafbaa41a14ee21150fe6d72f7ed14e88
Adam Sánchez <sanchez.portero.adam@alumnat.copernic.cat>
2022-10-17 21:31:44 +0200

    error manifest corregido

--- app/src/main/java/com/example/zenhabit/Splash_Screen.kt
+++ app/src/main/java/com/example/zenhabit/Splash_Screen.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import com.google.firebase.auth.FirebaseAuth
+
 
 class Splash_Screen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
