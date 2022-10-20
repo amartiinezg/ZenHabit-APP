f1344f0afc7d5b633e3579dc2d8533e15e3c6e17
Adam Sánchez <sanchez.portero.adam@alumnat.copernic.cat>
2022-10-20 22:19:00 +0200

    a

--- app/src/main/java/com/example/zenhabit/DiaryScreen.kt
+++ app/src/main/java/com/example/zenhabit/DiaryScreen.kt
@@ -1,10 +1,12 @@
 package com.example.zenhabit
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import app.futured.donut.DonutSection
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +28,10 @@ class DiaryScreen : Fragment() {
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+
         }
+
+
     }
 
     override fun onCreateView(
