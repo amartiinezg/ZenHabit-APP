fda5ecefd796ca5919f67b5983185fc4e9082446
Adam Sánchez <sanchez.portero.adam@alumnat.copernic.cat>
2022-10-21 18:43:21 +0200

    registro funcionando

--- app/src/main/java/com/example/zenhabit/MainActivity.kt
+++ app/src/main/java/com/example/zenhabit/MainActivity.kt
@@ -1,23 +1,41 @@
 package com.example.zenhabit
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.renderscript.ScriptGroup.Binding
 import android.view.Window
 import android.widget.Button
 import android.content.Intent
+import android.util.Log
+import android.widget.Toast
 import com.blogspot.atifsoftwares.animatoolib.Animatoo
+import com.example.zenhabit.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var bin: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        bin =   ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bin.root)
+
         var buttonRegister_LoginScreen = findViewById<Button>(R.id.btRegister_LoginScreen)
         var btForgotPassword_LoginScreen = findViewById<Button>(R.id.buttonResetPassword)
         var bt_login = findViewById<Button>(R.id.btLogin_LoginScreen)
+        var username = bin.inputUsernameLoginScreen.toString()
+        var password = bin.inputPasswordLoginScreen.toString()
+
+        auth = Firebase.auth
 
         buttonRegister_LoginScreen.setOnClickListener{
             val intent = Intent(this, RegistreActivity::class.java)
@@ -34,11 +52,32 @@ class MainActivity : AppCompatActivity() {
         }
 
         bt_login.setOnClickListener{
-            val intent = Intent(this, Home::class.java)
-            startActivity(intent)
-
-            Animatoo.animateSlideLeft(this)
+            IdentificarUsuari(username,password)
         }
+
+    }
+
+    private fun IdentificarUsuari(email: String, password: String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+
+
+                    val intent = Intent(this, PasswordCodeRequest::class.java)
+                    startActivity(intent)
+
+                    Animatoo.animateSlideLeft(this)
+
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+
+                }
+            }
 
     }
 
--- app/src/main/java/com/example/zenhabit/RegistreActivity.kt
+++ app/src/main/java/com/example/zenhabit/RegistreActivity.kt
@@ -1,17 +1,35 @@
 package com.example.zenhabit
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import com.blogspot.atifsoftwares.animatoolib.Animatoo
+import com.example.zenhabit.databinding.ActivityMainBinding
+import com.example.zenhabit.databinding.ActivityRegistreBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegistreActivity : AppCompatActivity() {
+    private lateinit var bin: ActivityRegistreBinding
+    private lateinit var auth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registre)
+        auth = Firebase.auth
+        bin =   ActivityRegistreBinding.inflate(layoutInflater)
+        setContentView(bin.root)
 
-        var btCancel = findViewById<Button>(R.id.btCancel_RegisterScreen)
+        var btCancel = bin.btCancelRegisterScreen
+        var btRegister = bin.btRegister
+
+
 
         btCancel.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
@@ -20,5 +38,34 @@ class RegistreActivity : AppCompatActivity() {
             Animatoo.animateSlideRight(this)
         }
 
+        btRegister.setOnClickListener{
+            var email = bin.inputEmailRegisterScreen.text.toString()
+            var password = bin.inputPasswordRegisterScreen.text.toString()
+            crearUsuari(email,password)
+        }
+
+    }
+
+    private fun crearUsuari(email: String, password: String) {
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+
+
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+
+                    Animatoo.animateSlideRight(this)
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Error en el registre :" + task.exception, Toast.LENGTH_SHORT).show()
+
+                }
+            }
     }
 }
