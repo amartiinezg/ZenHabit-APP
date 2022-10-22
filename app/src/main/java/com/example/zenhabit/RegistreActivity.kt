4e9819f2c6b64ead607fa646c4fca4cfc4bf4cc9
adamsanchez <sanchez.portero.adam@alumnat.copernic.cat>
2022-10-23 00:12:54 +0200

    tasca base de dades acabada

--- app/src/main/java/com/example/zenhabit/RegistreActivity.kt
+++ app/src/main/java/com/example/zenhabit/RegistreActivity.kt
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
 import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.blogspot.atifsoftwares.animatoolib.Animatoo
 import com.example.zenhabit.databinding.ActivityRegistreBinding
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import org.w3c.dom.Text
 
 class RegistreActivity : AppCompatActivity() {
     private lateinit var bin: ActivityRegistreBinding
@@ -26,7 +31,39 @@ class RegistreActivity : AppCompatActivity() {
 
         val btCancel = bin.btCancelRegisterScreen
         val btRegister = bin.btRegister
+        var editTextUsername = bin.inputUsernameRegisterScreen
+        var editTextPassword = bin.inputPasswordRegisterScreen
+        var editTextEmail = bin.inputEmailRegisterScreen
+        var layoutUsername = bin.usernameLayoutRegister
+        var layoutPassword = bin.passwordLayoutRegister
+        var layoutEmail = bin.emailLayout
+        var tvsnackbar = bin.snackbarRegister
 
+        //Focused Listeners
+
+        editTextUsername.onFocusChangeListener = View.OnFocusChangeListener { view, hasFocus ->
+            if (hasFocus) {
+                layoutUsername.error = null
+            } else {
+
+            }
+        }
+
+        editTextPassword.onFocusChangeListener = View.OnFocusChangeListener { view, hasFocus ->
+            if (hasFocus) {
+                layoutPassword.error = null
+            } else {
+
+            }
+        }
+
+        editTextEmail.onFocusChangeListener = View.OnFocusChangeListener { view, hasFocus ->
+            if (hasFocus) {
+                layoutEmail.error = null
+            } else {
+
+            }
+        }
 
 
         btCancel.setOnClickListener{
@@ -36,15 +73,48 @@ class RegistreActivity : AppCompatActivity() {
             Animatoo.animateSlideRight(this)
         }
 
-        btRegister.setOnClickListener{
-            val email = bin.inputEmailRegisterScreen.text.toString()
+        btRegister.setOnClickListener {
+            val nom = bin.inputUsernameRegisterScreen.text.toString()
             val password = bin.inputPasswordRegisterScreen.text.toString()
-            crearUsuari(email,password)
+            val email = bin.inputEmailRegisterScreen.text.toString()
+
+            if ((email == "") && (password == "") && (nom == "")) {
+                layoutEmail.error = getString(R.string.errorNullEmail)
+                layoutPassword.error = getString(R.string.errorNullPassword)
+                layoutUsername.error = getString(R.string.errorNullUsername)
+
+            } else if ((email == "") && (password == "")) {
+                layoutEmail.error = getString(R.string.errorNullEmail)
+                layoutEmail.error = getString(R.string.errorNullEmail)
+            } else if ((password == "") && (nom == "")) {
+                layoutPassword.error = getString(R.string.errorNullPassword)
+                layoutUsername.error = getString(R.string.errorNullUsername)
+            } else if ((email == "") && (nom == "")) {
+                layoutEmail.error = getString(R.string.errorNullEmail)
+                layoutUsername.error = getString(R.string.errorNullUsername)
+
+            }else if ((nom == "") && (password == "")){
+                layoutPassword.error = getString(R.string.errorNullPassword)
+                layoutUsername.error = getString(R.string.errorNullUsername)
+
+            }else if (email==""){
+                layoutEmail.error = getString(R.string.errorNullEmail)
+
+            }else if (nom==""){
+            layoutUsername.error = getString(R.string.errorNullUsername)
+
+            }else if (password == "") {
+            layoutPassword.error = getString(R.string.errorNullPassword)
+
+            }else {
+                crearUsuari(nom, password, tvsnackbar)
+            }
+            Utilities(editTextUsername, editTextPassword, editTextEmail)
         }
 
     }
 
-    private fun crearUsuari(email: String, password: String) {
+    private fun crearUsuari(email: String, password: String, tvsnackbar : TextView) {
 
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
@@ -59,12 +129,22 @@ class RegistreActivity : AppCompatActivity() {
                     Animatoo.animateSlideRight(this)
 
                 } else {
-                    // If sign in fails, display a message to the user.
+                    // If register, display a message to the user.
                     Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                    Toast.makeText(baseContext, "Error en el registre :" + task.exception, Toast.LENGTH_SHORT).show()
+                    Snackbar.make(tvsnackbar,"Error. Revisa els camps introduits", Snackbar.LENGTH_LONG)
+                        .show()
 
                 }
             }
+    }
+
+    fun Utilities(edittextUsername: EditText, editTextPassword: EditText, editTextEmail : EditText){
+
+        edittextUsername.clearFocus()
+        editTextPassword.clearFocus()
+        editTextEmail.clearFocus()
+        editTextPassword.text?.clear()
+
     }
 
 
