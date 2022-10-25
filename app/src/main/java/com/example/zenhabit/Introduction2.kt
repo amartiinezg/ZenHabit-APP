0213e4f4fa4444936f59acbfd4f98692cb160afa
Àngel Martínez González <martinez.gonzalez.angel@alumnat.copernic.cat>
2022-10-25 15:42:43 +0200

    Canvis realitzats.

--- app/src/main/java/com/example/zenhabit/Introduction2.kt
+++ app/src/main/java/com/example/zenhabit/Introduction2.kt
@@ -13,13 +13,10 @@ class Introduction2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_introduction_2)
         val butonNext_Introduction2 = findViewById<Button>(R.id.bt_Next_Introduction)
-
-            butonNext_Introduction2.setOnClickListener{
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-
-                Animatoo.animateSlideLeft(this)
-            }
-
+        butonNext_Introduction2.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            Animatoo.animateSlideLeft(this)
+        }
     }
 }
